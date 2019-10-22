33b1a3bf8ca7eb6dad643721aca8a243d2185a40
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-22 21:10:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,12 +213,11 @@ end
 def winning_team
   home = 0
   away = 0
-    game_hash[:home][:players].each {|location, team_data|
-      binding.pry
-      home += team_data[:points]
+    game_hash[:home][:players].each {|location|
+      home += location[:points]
     }
-    game_hash[:away][:players].each {|location, team_data|
-      away += team_data[:points]
+    game_hash[:away][:players].each {|location|
+      away += location[:points]
     }
     if home > away
       game_hash[:home][:team_name]
